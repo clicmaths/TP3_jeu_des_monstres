6a49e77a87acdbb76010a3f151bfb17dab037d53
Yeseong Cheong <cheongy@educ.college-montreal.qc.ca>
2023-10-25 14:55:00 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#variable
+import random
+
+point_vie = "20"
+force_adversaire = random.randint(1,5)
+score_dé = random.randint(1,6)
+nombre_victoire = if score_dé > force_adversaire:
+                    nombre_victoire +1
+print(""+str(force_adversaire)+"")
+
